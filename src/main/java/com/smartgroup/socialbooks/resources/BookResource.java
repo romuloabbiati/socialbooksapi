198779870e8013fa534b0a58d4585a0904b0cf49
romuloabbiati <rhabbiati@hotmail.com>
2022-12-20 05:36:25 +0000

    BookResource.save()

--- src/main/java/com/smartgroup/socialbooks/resources/BookResource.java
+++ src/main/java/com/smartgroup/socialbooks/resources/BookResource.java
@@ -3,6 +3,7 @@ package com.smartgroup.socialbooks.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +12,20 @@ import com.smartgroup.socialbooks.domain.Book;
 import com.smartgroup.socialbooks.repository.BookRepository;
 
 @RestController
+@RequestMapping(path = "/books")
 public class BookResource {
 	
 	@Autowired
 	private BookRepository bookRepository;
 
-	@RequestMapping(path = "/books", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public List<Book> findAll() {
 		return bookRepository.findAll();
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public void save(@RequestBody Book book) {
+		bookRepository.save(book);
 	}
 	
 }
