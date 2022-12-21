96e12f1988594dba2bde63c5aa9b5ee12d5f2fc4
romuloabbiati <rhabbiati@hotmail.com>
2022-12-21 05:18:32 +0000

    BookResource.findById()

--- src/main/java/com/smartgroup/socialbooks/resources/BookResource.java
+++ src/main/java/com/smartgroup/socialbooks/resources/BookResource.java
@@ -1,8 +1,10 @@
 package com.smartgroup.socialbooks.resources;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,6 +23,13 @@ public class BookResource {
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Book> findAll() {
 		return bookRepository.findAll();
+	}
+	
+	@RequestMapping(path = "/{id}", method = RequestMethod.GET)
+	public Book findById(@PathVariable Long id) {
+		Optional<Book> bookOptional = bookRepository.findById(id);
+		return bookOptional.orElseThrow(
+				() -> new RuntimeException("Book with id " + id + " not found!"));
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
